66e7c7a1c841f0ac9d34a05b9ce7154ed46d9273
Maciej A. Czyzewski <mail@maciejczyzewski.me>
2016-10-09 11:38:35 +0200

    [modified]: cleaning up code

--- main.cpp
+++ main.cpp
@@ -9,7 +9,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
+ * The above copyright notice and this permission notice shall be included in
+ * all
  * copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -21,8 +22,9 @@
  * SOFTWARE.
 */
 
-//I defined system headers and macros here.
-//Because, 'wifi_deauth.h' and 'wifi_deauth.cpp' must be system independent module.
+// I defined system headers and macros here.
+// Because, 'wifi_deauth.h' and 'wifi_deauth.cpp' must be system independent
+// module.
 #if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -31,17 +33,17 @@
 #elif defined(__WIN32)
 #endif
 
-#include <cstdlib>
 #include "wifi_deauth.h"
+#include <cstdlib>
 
-inline int chkWiFiChannel(int _channel){
-    //Not compatible to 5Ghz 802.11 Yet
-    if(_channel < 1 || _channel > 14)
-        return 1;
-    return _channel;
+inline int chkWiFiChannel(int _channel) {
+  // Not compatible to 5Ghz 802.11 Yet
+  if (_channel < 1 || _channel > 14)
+    return 1;
+  return _channel;
 }
 
-bool isExistDirectory(const char* nic_name) {
+bool isExistDirectory(const char *nic_name) {
 #if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
   struct stat sb;
   char path_name[100];
@@ -62,14 +64,19 @@ bool isExistDirectory(const char* nic_name) {
 #endif
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   if (argc < 4) {
-    std::cout << "[*] Usage : " << argv[0] << " [Monitor Interface] [Managed Interface] [WiFi Channel]" << std::endl;
-    std::cout << "    If you make a wrong input to 'WiFi Channel', channel is setted by 1." << std::endl;
+    std::cout << "[*] Usage : " << argv[0]
+              << " [Monitor Interface] [Managed Interface] [WiFi Channel]"
+              << std::endl;
+    std::cout << "    If you make a wrong input to 'WiFi Channel', channel is "
+                 "setted by 1."
+              << std::endl;
     return -1;
   }
 
-  if (isExistDirectory(argv[1]) == false || isExistDirectory(argv[2]) == false) {
+  if (isExistDirectory(argv[1]) == false ||
+      isExistDirectory(argv[2]) == false) {
     std::cout << "[*] Input interface is not exist." << std::endl;
     std::cout << "    Please check your interface." << std::endl;
     return -1;
